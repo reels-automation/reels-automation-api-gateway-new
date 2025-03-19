d94104a1d993eda3e6a88b19dcdb9d836ae8f2e5
darthpedroo <pedrovillarino06@gmail.com>
2025-03-19 16:17:56 -0300

    Added requirements, readme , makefile and env.template

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Reels Automation Api Gateway
+
+## Insalación
+
+Iniciar un virtual environment con:
+
+virtualenv env
+
+Ejecutar make install para instalar los modulos de requirements.txt
+
+## Ejecución
+
+Ejecutar make python-run
--- server_base.py
+++ server_base.py
@@ -1,13 +1,14 @@
+import os
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+print("DATABASE URL", DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 
 Base = declarative_base()
