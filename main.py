d30f87eb30139a7d4b41e421f6bfb42378ee332b
Santino Andreatta <149499721+CuackXI@users.noreply.github.com>
2025-03-26 01:31:54 -0300

    Agregué el middleware para poder pasar el CORS

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from blueprints.login.login import login_router
 from blueprints.register.register import register_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"],  # Puedes restringirlo a dominios específicos en producción
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
+
 app.include_router(login_router)
 app.include_router(register_router)
