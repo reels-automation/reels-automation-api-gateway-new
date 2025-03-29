8f431e02376ccf21793097f57d337153d1baf081
Galo <nebulafa@gmail.com>
2025-03-29 12:19:13 -0300

    added boilerplate data to test postman

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-from utils.jwt_utils import create_access_token
-
-data = {"username":"gogagoga", "password":"gogagoga"}
-token = create_access_token(data)
-
-print("Token: ", token)
-
+data = {
+        "tema": "Quien es gogo",
+        "personaje": "Homero Simpson",
+        "script": "",
+        "tts_audio_name": "",
+        "tts_audio_bucket": "",
+        "subtitles_name": "",
+        "subtitles_bucket": "",
+        "author": "Mi amigo el galofa",
+        "pitch": "0",
+        "tts_voice": "es-ES-XimenaNeural",
+        "tts_rate": "0",
+        "pth_voice": "HOMERO SIMPSON LATINO",
+        "instagram_account": "aprendiendo.con.personajes",
+        "gameplay_name": ""
+    }
