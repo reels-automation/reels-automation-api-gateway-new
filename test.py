687125a9ff6b5e0d166725ab5bd736067f9be0d6
darthpedroo <pedrovillarino06@gmail.com>
2025-04-26 10:42:30 -0300

    added postman template

--- test.py
+++ test.py
@@ -1,16 +1,48 @@
 data = {
-        "tema": "Quien es gogo",
-        "personaje": "Homero Simpson",
-        "script": "",
-        "tts_audio_name": "",
-        "tts_audio_bucket": "",
-        "subtitles_name": "",
-        "subtitles_bucket": "",
-        "author": "Mi amigo el galofa",
-        "pitch": "0",
-        "tts_voice": "es-ES-XimenaNeural",
-        "tts_rate": "0",
-        "pth_voice": "HOMERO SIMPSON LATINO",
-        "instagram_account": "aprendiendo.con.personajes",
-        "gameplay_name": ""
+  "tema": "Explica la programacion orientada a dops",
+  "usuario": 1,
+  "idioma": "es",
+  "personaje": "Homero Simpson",
+  "script": "",
+  "audio_item": [
+    {
+      "tts_audio_name": "",
+      "tts_audio_directory": "",
+      "file_getter": "",
+      "pitch": 0,
+      "tts_voice": "es-ES-XimenaNeural",
+      "tts_rate": 0,
+      "pth_voice": "HOMERO SIMPSON LATINO"
     }
+  ],
+  "subtitle_item": [
+    {
+      "subtitles_name": "",
+      "file_getter": "",
+      "subtitles_directory": ""
+    }
+  ],
+  "author": "Mi amigo el galofa",
+  "gameplay_name": "",
+  "background_music": [
+    {
+        "audio_name": "",
+        "file_getter": "",
+        "start_time": 0,
+        "duration": 100
+    }
+  ],
+  "images": [
+    {
+        "image_name": "homero1.png",
+        "image_modifier": "rotate",
+        "file_getter": "",
+        "image_directory": "local",
+        "timestamp": 0,
+        "duration": 10
+    }
+  ],
+  "random_images": false,
+  "random_amount_images": 0,
+  "gpt_model": "llama3.2:3b"
+}
